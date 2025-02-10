71d48d5b882aa152cafaffe53cc4d12bbeae496e
Stan-North <spine201150@gmail.com>
2025-02-11 01:30:30 +0300

    fixed exception messages

--- src/main/java/com/javaacademy/cinema/common/ErrorMessages.java
+++ src/main/java/com/javaacademy/cinema/common/ErrorMessages.java
@@ -2,6 +2,12 @@ package com.javaacademy.cinema.common;
 
 public class ErrorMessages {
     public static final String TICKET_ALREADY_BOUGHT = "Билет с указанным id уже куплен!";
+    public static final String TICKET_DOES_NOT_EXIST = "Билет с таким id не существует";
     public static final String NO_ONE_ROWS_UPDATED = "Не была обновлена ни одна строка";
     public static final String INVALID_USER_TOKEN = "Неверный токен пользователя";
+    public static final String SEAT_DOES_NOT_EXIST = "Место с таким названием не существует";
+    public static final String SESSION_DOES_NOT_EXIST = "Сессия с таким id не существует";
+    public static final String DATA_INTEGRITY_VIOLATION = "Нарушение целостности данных: ";
+    public static final String INVALID_ARGUMENTS = "Неверные аргументы:";
+    public static final String UNEXPECTED_ERROR = "Произошла непредвиденная ошибка: ";
 }
--- src/main/java/com/javaacademy/cinema/exception/GlobalExceptionHandler.java
+++ src/main/java/com/javaacademy/cinema/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.javaacademy.cinema.exception;
 
+import com.javaacademy.cinema.common.ErrorMessages;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,27 +12,28 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(DataIntegrityViolationException.class)
     public ResponseEntity<String> handleDataIntegrityViolation(DataIntegrityViolationException e) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Data integrity violation: " + e.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ErrorMessages.DATA_INTEGRITY_VIOLATION
+                + e.getMessage());
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid arguments: " + e.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ErrorMessages.INVALID_ARGUMENTS + e.getMessage());
     }
 
     @ExceptionHandler(SessionDoesNotExistException.class)
     public ResponseEntity<String> handleSessionDoesNotExistException(Exception e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Сессия с таким id не существует");
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ErrorMessages.SESSION_DOES_NOT_EXIST);
     }
 
     @ExceptionHandler(TicketDoesNotExistException.class)
     public ResponseEntity<String> handleTicketDoesNotExistException(Exception e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Билет с таким id не существует");
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ErrorMessages.TICKET_DOES_NOT_EXIST);
     }
 
     @ExceptionHandler(SeatDoesNotExistException.class)
     public ResponseEntity<String> handleSeatDoesNotExistException(Exception e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Место с таким названием не существует");
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ErrorMessages.SEAT_DOES_NOT_EXIST);
     }
 
     @ExceptionHandler(TicketStatusException.class)
@@ -41,7 +43,8 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleGeneralException(Exception e) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An unexpected error occurred: " + e.getMessage());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ErrorMessages.UNEXPECTED_ERROR
+                + e.getMessage());
     }
 
 }
